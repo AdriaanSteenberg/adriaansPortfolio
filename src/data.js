1220e63c1e209ef8366bef2eee05c50cc9d5ef10
Adriaan Steenberg <adriaansteenberg1@gmail.com>
2022-11-21 16:43:15 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const projects = [
+  {
+    title: "Teastation ",
+    subtitle: "CSS and Javascript",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.jpg",
+    link: "https://teastationproject2.netlify.app",
+  },
+  {
+    title: "Cocktails ",
+    subtitle: "React",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://react-project1-cocktails.netlify.app/",
+  },
+  {
+    title: "Github user",
+    subtitle: "React ",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://project4-githubuser-search.netlify.app",
+  },
+  {
+    title: "Travel App",
+    subtitle: "Javascript and CSS",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://backroadsproject.netlify.app",
+  },
+{
+    title: "Comfy Store",
+    subtitle: "Javascript and CSS",
+    description:
+      "A store app that you can choose and be happy with new furniture. Was a fun project to accomplish.",
+    image: "./project-5.gif",
+    link: "https://project3confystore.netlify.app/",
+  },
+  
+  {
+    title: "Tin Dog",
+    subtitle: "Javascript and CSS",
+    description:
+      "This is an app like facebook.",
+    image: "./project-6.gif",
+    link: "https://project3confystore.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React.js",
+  "CSS",
+  "Node",
+  "GraphQL",
+  "HTML",
+];
